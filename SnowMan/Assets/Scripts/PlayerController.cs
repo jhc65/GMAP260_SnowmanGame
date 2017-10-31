e63c19c2fef5587fab10ef5a566c5605da75f782
ztcric <tz85@drexel.edu>
2017-10-31 15:55:28 -0400

    player movement and camera rotation

---
+++ SnowMan/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float rotateSpeed;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetKey(KeyCode.LeftArrow)){
+		//	transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+
+		//}
+
+		//if (Input.GetKey(KeyCode.RightArrow)){
+		//	transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+
+		//}
+
+
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += transform.forward * Time.deltaTime*speed;
+		} 
+		else if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
+			transform.position -= transform.forward * Time.deltaTime*speed;
+		} 
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (0,  -rotateSpeed * Time.deltaTime, 0);
+		} 
+		else if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (0,  rotateSpeed * Time.deltaTime, 0);
+		}
+			//transform.Rotate(0.0f,Time.deltaTime*-rotateSpeed, 0,0f);
+	}
+
+	void FixedUpdate()  // called each physics steps
+	{
+
+//		float moveHorizontal = Input.GetAxis ("Horizontal"); // default axis : Horizontal, vertical
+//		float moveVertical = Input.GetAxis ("Vertical");
+//		Vector3 movement = new Vector3 (0.0f, 0.0f, moveVertical);
+		//rb.velocity = movement*speed;
+
+
+	}
+}
