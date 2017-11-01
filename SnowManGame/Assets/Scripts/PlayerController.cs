cf1bdefb4fa3e331e42e56094ceafb2b7c0a15a6
Daniel Perlman <dmp359@drexel.edu>
2017-11-01 00:12:11 -0400

    snowman game with updated fixes and tweaks

--- SnowManGame/Assets/Scripts/PlayerController.cs
+++ SnowManGame/Assets/Scripts/PlayerController.cs
@@ -10,12 +10,24 @@ public class PlayerController : MonoBehaviour {
 	public Transform shotSpawn;
 	public float fireRate;
 
-	private static int numBullets = 30;
+	private static int numBullets = 20;
 	private GameObject[] bullets = new GameObject[numBullets];
 	private float nextFire = 0;
 	private int nextBullet;
+	private float bulletSpeed = 20f;
 
 	private bool canShoot = true;
+	private int hp = 1;
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.tag == "Enemy") {
+			hp--;
+			if (hp <= 0) { // dead, so stop spawning and "destroy" player
+				GameObject.FindGameObjectWithTag("Spawner").GetComponent<EnemySpawner>().Disable();
+				gameObject.SetActive(false);
+			}
+		}
+	}
 
 	void Start () {
 
@@ -39,13 +51,14 @@ public class PlayerController : MonoBehaviour {
 			bullet.SetActive(true);
 			bullet.transform.position = shotSpawn.position;
 			bullet.transform.rotation = shotSpawn.rotation;
+			bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
 		}
 
 		if (Input.GetAxis("Vertical") > 0) {
-			transform.position += transform.forward * Time.deltaTime*speed;
+			transform.position += transform.forward * Time.deltaTime * speed;
 		} 
 		if (Input.GetAxis("Vertical") < 0) {
-			transform.position -= transform.forward * Time.deltaTime*speed;
+			transform.position -= transform.forward * Time.deltaTime * speed;
 		} 
 		if (Input.GetAxis("Horizontal") < 0) {
 			transform.Rotate (0,  -rotateSpeed, 0);
