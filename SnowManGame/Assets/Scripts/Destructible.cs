0e224e14abd408489d7f6567b6787bfea65d26f4
Daniel Perlman <dmp359@drexel.edu>
2017-11-14 15:33:58 -0500

    DESTROY TREES

---
+++ SnowManGame/Assets/Scripts/Destructible.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+	public GameObject destroyedVersion;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// If a snowball hits a tree, spawn the destroyed one and remove the old one
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.CompareTag("Projectile")) {
+			Instantiate(destroyedVersion, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
