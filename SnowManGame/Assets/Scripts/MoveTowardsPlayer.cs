259602e5b6fdd20b013b5603e06d633c071d0cf6
Daniel Perlman <dmp359@drexel.edu>
2017-12-06 16:05:49 -0500

    Nav mesh and bunnies now move with Unity AI

--- SnowManGame/Assets/Scripts/MoveTowardsPlayer.cs
+++ SnowManGame/Assets/Scripts/MoveTowardsPlayer.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MoveTowardsPlayer : MonoBehaviour {
 
 	private Transform target;
     public float speed = 5f;
 	private int hp = 1;
+	private NavMeshAgent agent;
 	private Color hitColor = new Color(255f,0f,0f);
 		
 	// get audio src snowball-impact-rabbit.wav
@@ -15,6 +17,7 @@ public class MoveTowardsPlayer : MonoBehaviour {
 
     void Start () {
 		target = GameObject.FindGameObjectWithTag("Player").transform;
+		agent = GetComponent<NavMeshAgent>();
 
 		// init audio
 		audio = GetComponent<AudioSource> ();
@@ -22,9 +25,14 @@ public class MoveTowardsPlayer : MonoBehaviour {
 
 	void Update () {
 
+
+		// Head towards player along nav mesh
+		agent.destination = target.position; 
+
+
 		// Head in a straight line towards player
-		Vector3 targetPosition = target.position;
-		transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+		//Vector3 targetPosition = target.position;
+		//transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
     }
 
 	void OnCollisionEnter(Collision collision) {
